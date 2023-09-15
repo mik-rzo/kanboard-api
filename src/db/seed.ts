efe46c57fb7565dfd7e78b1e5c8c12d21f49d652
mik-rzo <michael.panong3@gmail.com>
2023-09-15 21:30:09 +0100

    fixed user array mutating between tests

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,25 +7,24 @@ interface UserI {
 	password: string
 }
 
-const users: UserI[] = [
-	{
-		fullName: 'Zara Russel',
-		email: 'zara.russel@example.com',
-		password: 'fddnQzxuqerp'
-	},
-	{
-		fullName: 'Gabi Ramsay',
-		email: 'gabi.ramsay@example.com',
-		password: '2Vbikjlwe7wo'
-	},
-	{
-		fullName: 'Saul Goodman',
-		email: 'saul.goodman@example.com',
-		password: 'imfeym7q9nwj'
-	}
-]
-
 export async function seed() {
+	const users: UserI[] = [
+		{
+			fullName: 'Zara Russel',
+			email: 'zara.russel@example.com',
+			password: 'fddnQzxuqerp'
+		},
+		{
+			fullName: 'Gabi Ramsay',
+			email: 'gabi.ramsay@example.com',
+			password: '2Vbikjlwe7wo'
+		},
+		{
+			fullName: 'Saul Goodman',
+			email: 'saul.goodman@example.com',
+			password: 'imfeym7q9nwj'
+		}
+	]
 	const db = await pool
 	for (let i = 0; i < users.length; i++) {
 		users[i].password = await bcrypt.hash(users[i].password, 10)
