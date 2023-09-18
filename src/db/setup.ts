cbe8e3d9fa6d83fd842070c6b09ad8df0adf6f60
mik-rzo <michael.panong3@gmail.com>
2023-09-18 19:09:31 +0100

    changed user document validation to include workspaces

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -9,7 +9,7 @@ export async function setup() {
 			$jsonSchema: {
 				bsonType: 'object',
 				title: 'User Object Validation',
-				required: ['fullName', 'email', 'password'],
+				required: ['fullName', 'email', 'password', 'workspaces'],
 				properties: {
 					fullName: {
 						bsonType: 'string',
@@ -22,6 +22,25 @@ export async function setup() {
 					password: {
 						bsonType: 'string',
 						description: "'password' must be a string and is required"
+					},
+					workspaces: {
+						bsonType: 'array',
+						minItems: 1,
+						description: "'workspaces' must be an array and must contain at least one item",
+						items: {
+							bsonType: 'object',
+							required: ['workspaceId', 'workspaceName'],
+							properties: {
+								workspaceId: {
+									bsonType: 'objectId',
+									description: "'workspaceId' must be a unique object ID and is required"
+								},
+								workspaceName: {
+									bsonType: 'string',
+									description: "'workspaceName' must be a string and is required"
+								}
+							}
+						}
 					}
 				}
 			}
