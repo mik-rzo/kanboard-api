ea51b64b47b0a0040769e5a22147bd05f7c2356a
mik-rzo <michael.panong3@gmail.com>
2023-10-18 14:19:31 +0100

    modified database schema

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -2,15 +2,15 @@ import pool from './connection.js'
 
 export async function setup() {
 	const db = await pool
-	await db.collection('users').drop()
-	await db.collection('workspaces').drop()
 	await db.collection('sessions').drop()
+	await db.collection('workspaces').drop()
+	await db.collection('users').drop()
 	await db.createCollection('users', {
 		validator: {
 			$jsonSchema: {
 				bsonType: 'object',
 				title: 'User Object Validation',
-				required: ['fullName', 'email', 'password', 'workspaces'],
+				required: ['fullName', 'email', 'password'],
 				properties: {
 					fullName: {
 						bsonType: 'string',
@@ -23,32 +23,32 @@ export async function setup() {
 					password: {
 						bsonType: 'string',
 						description: "'password' must be a string and is required"
-					},
-					workspaces: {
-						bsonType: 'array',
-						description: "'workspaces' must be an array",
-						items: {
-							bsonType: 'string'
-						}
-					}
-				}
-			}
-		}
-	})
-	await db.createCollection('workspaces', {
-		validator: {
-			$jsonSchema: {
-				bsonType: 'object',
-				title: 'Workspace Object Validation',
-				required: ['name'],
-				properties: {
-					name: {
-						bsonType: 'string',
-						description: "'name' must be a string and is required"
 					}
 				}
 			}
 		}
 	})
 	await db.collection('users').createIndex({ email: 1 }, { unique: true, name: 'email_' })
+	await db.createCollection('workspaces', {
+		validator: {
+			$jsonSchema: {
+				bsonType: 'object',
+				title: 'Workspace Object Validation',
+				required: ['name', 'users'],
+				properties: {
+					name: {
+						bsonType: 'string',
+						description: "'name' must be a string and is required"
+					},
+					users: {
+						bsonType: 'array',
+						description: "'users' must be an array and is required",
+						items: {
+							bsonType: 'objectId'
+						}
+					}
+				}
+			}
+		}
+	})
 }
