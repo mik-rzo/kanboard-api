1ff40f94a9281bed3a9ac512fd4d1838352b1846
mik-rzo <michael.panong3@gmail.com>
2023-11-22 14:14:25 +0000

    added board document schema

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -3,6 +3,7 @@ import pool from './connection.js'
 export async function setup() {
 	const db = await pool
 	await db.collection('sessions').drop()
+	await db.collection('boards').drop()
 	await db.collection('workspaces').drop()
 	await db.collection('users').drop()
 	await db.createCollection('users', {
@@ -14,15 +15,15 @@ export async function setup() {
 				properties: {
 					fullName: {
 						bsonType: 'string',
-						description: "'name' must be a string and is required"
+						description: 'must be a string and is required'
 					},
 					email: {
 						bsonType: 'string',
-						description: "'email' must be a string and is required"
+						description: 'must be a string and is required'
 					},
 					password: {
 						bsonType: 'string',
-						description: "'password' must be a string and is required"
+						description: 'must be a string and is required'
 					}
 				}
 			}
@@ -38,20 +39,115 @@ export async function setup() {
 				properties: {
 					name: {
 						bsonType: 'string',
-						description: "'name' must be a string and is required"
+						description: 'must be a string and is required'
 					},
 					users: {
 						bsonType: 'array',
-						description: "'users' must be an array and is required",
+						description: 'must be an array and is required',
 						items: {
-							bsonType: 'objectId'
+							bsonType: 'objectId',
+							description: 'must be an objectId'
 						}
 					},
 					boards: {
 						bsonType: 'array',
-						description: '"boards" must be an array and is required',
+						description: 'must be an array and is required',
 						items: {
-							bsonType: 'objectId'
+							bsonType: 'objectId',
+							description: 'must be an objectId'
+						}
+					}
+				}
+			}
+		}
+	})
+	await db.createCollection('boards', {
+		validator: {
+			$jsonSchema: {
+				bsonType: 'object',
+				title: 'Board Object Validation',
+				required: ['name', 'labels', 'lists'],
+				properties: {
+					name: {
+						bsonType: 'string',
+						description: 'must be a string and is required'
+					},
+					labels: {
+						bsonType: 'array',
+						description: 'must be an array and is required',
+						items: {
+							bsonType: 'object',
+							required: ['_id', 'colour', 'title'],
+							properties: {
+								_id: {
+									bsonType: 'objectId',
+									description: 'must be an objectId and is required'
+								},
+								colour: {
+									bsonType: 'string',
+									description: 'must be a string and is required'
+								},
+								title: {
+									bsonType: 'string',
+									description: 'must be a string and is required'
+								}
+							}
+						}
+					},
+					lists: {
+						bsonType: 'array',
+						description: 'must be an array and is required',
+						items: {
+							bsonType: 'object',
+							required: ['_id', 'header', 'cards'],
+							properties: {
+								_id: {
+									bsonType: 'objectId',
+									description: 'must be an objectId and is required'
+								},
+								header: {
+									bsonType: 'string',
+									description: 'must be a string and is required'
+								},
+								cards: {
+									bsonType: 'array',
+									description: 'must be an array and is required',
+									items: {
+										bsonType: 'object',
+										required: ['_id', 'title', 'description', 'assign', 'labels'],
+										properties: {
+											_id: {
+												bsonType: 'objectId',
+												description: 'must be an objectId and is required'
+											},
+											title: {
+												bsonType: 'string',
+												description: 'must be a string and is required'
+											},
+											description: {
+												bsonType: 'string',
+												description: 'must be a string and is required'
+											},
+											assign: {
+												bsonType: 'array',
+												description: 'must be an array and is required',
+												items: {
+													bsonType: 'objectId',
+													description: 'must be an objectId'
+												}
+											},
+											labels: {
+												bsonType: 'array',
+												description: 'must be an array and is required',
+												items: {
+													bsonType: 'objectId',
+													description: 'must be an objectId'
+												}
+											}
+										}
+									}
+								}
+							}
 						}
 					}
 				}
